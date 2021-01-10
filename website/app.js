94b95aacae2ec7f0bad4dd1cd163610a49341cb6
clow <claudioramirezjimenez@gmail.com>
2021-01-10 14:30:13 +0000

    Syntax fixing

--- website/app.js
+++ website/app.js
@@ -1,11 +1,10 @@
-// Post Data
 const postData = async ( url = '', data = {})=>{
     console.log(data)
         const response = await fetch(url,{
     method: 'POST',
     credentials: 'same-origin',
     headers: {
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
 });
